23f0f2a5cf712e947be37b8ea75c2794072b8a02
jono <jono2496@gmail.com>
2024-09-02 06:58:06 -0700

    update dep and starship

--- home.nix
+++ home.nix
@@ -111,19 +111,17 @@ in
     nix-index-database.comma.enable = true;
 
     # FIXME: disable this if you don't want to use the starship prompt
-    starship.enable = true;
-    starship.settings = {
-      aws.disabled = true;
-      gcloud.disabled = true;
-      kubernetes.disabled = false;
-      git_branch.style = "242";
-      directory.style = "blue";
-      directory.truncate_to_repo = false;
-      directory.truncation_length = 8;
-      python.disabled = true;
-      ruby.disabled = true;
-      hostname.ssh_only = false;
-      hostname.style = "bold green";
+    starship = {
+      enable = true;
+      settings = {
+        directory = {
+          truncate_to_repo = true;
+          style = "italic blue";
+          repo_root_style = "bold blue";
+
+        };
+        hostname.ssh_only = true;
+      };
     };
 
     # FIXME: disable whatever you don't want
